1bcd451c0eb9a87b57bf93f96a6804141953ae72
acsaralegui <aaron.saralegui@gmail.com>
2020-06-29 00:04:43 +0000

    falcon theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,22 @@
 //= require tether
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+
+
+
+jQuery( document ).ready( function($) {
+  $( window ).scroll( function () {
+    if ( $(document).scrollTop() > 150 ) {
+      // Navigation Bar
+      $('.navbar').removeClass('fadeIn');
+      $('body').addClass('shrink');
+      $('.navbar').addClass('animated fadeInDown');
+    } else {
+      $('.navbar').removeClass('fadeInDown');
+      $('body').removeClass('shrink');
+      $('.navbar').addClass('animated fadeIn');
+    }
+  });
+});
